3401106411c8569a2ec6de1fb90dd165b4870cbf
Johny-loh <71099202+Johny-loh@users.noreply.github.com>
2022-05-13 00:22:30 +0300

    Update train.py

--- train.py
+++ train.py
@@ -13,8 +13,11 @@ class Train:
         parking = time()
 
         while len(self.capacity) < TRAIN_MAX_CAPACITY and (time() - parking < DELAY * S):
-            passenger = await STATIONS[self.station][self.direction]['q'].get()
-            self.capacity.append(passenger)
+            try:
+                passenger = STATIONS[self.station][self.direction]['q'].get_nowait()
+                self.capacity.append(passenger)
+            except queues.QueueEmpty:
+                pass
 
         await sleep((DELAY - (time() - parking)) * S)
 
@@ -22,9 +25,10 @@ class Train:
         while 1:
             await self.landing()
 
-            for _ in range(STATIONS[self.station][self.direction]['t']):
-                self.pos += 1 - 2 * (self.direction == 'l')
-                await sleep(S * 60)
+            for _ in range(STATIONS[self.station][self.direction]['t'] * 60):
+                if _ % 60 == 59:
+                    self.pos += 1 - 2 * (self.direction == 'l')
+                await sleep(S)
 
             self.station += 1 - 2 * (self.direction == 'l')
             self.change_direction()
